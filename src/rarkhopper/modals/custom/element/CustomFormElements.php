bd4cb284ece6554b3f86b9ebe834195d9745c064
Rark7040 <crimera.hello@gmail.com>
2023-01-04 04:33:09 +0900

    add custom form element

--- src/rarkhopper/modals/custom/element/CustomFormElements.php
+++ src/rarkhopper/modals/custom/element/CustomFormElements.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 namespace rarkhopper\modals\custom\element;
 
-class CustomFormElements{
+use rarkhopper\modals\FormElements;
+use rarkhopper\modals\utils\FormType;
 
+class CustomFormElements extends FormElements{
+	private CustomFormOptions $options;
+
+	public function __construct(string $title, CustomFormOptions $options){
+		parent::__construct($title);
+		$this->options = $options;
+		$this->initElement();
+	}
+
+	private function initElement() : void{
+		$this->appendElement(new FormType(FormType::TYPE_CUSTOM));
+		$this->appendElement($this->options);
+	}
+
+	public function getOptions() : CustomFormOptions{
+		return $this->options;
+	}
 }
